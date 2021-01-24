5e88848ce0821e65fe3c36102696a0c18489eaa2
Eugene Lisovsky <mult@multed.com>
2021-01-24 15:26:27 +0700

    add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "posts#index"
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   # get 'posts/index'
   # get 'posts/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
